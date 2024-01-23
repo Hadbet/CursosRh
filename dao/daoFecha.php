2d5931076cb7f479b2af19da3956e04bc50d4e3c
hadbet <hadbet.altamirano@grammer.com>
2024-01-23 17:06:59 -0600

    Carga inicial de plantilla

---
+++ dao/daoFecha.php
@@ -0,0 +1,21 @@
+<?php
+
+include_once('db/db_RH.php');
+
+ContadorApu();
+
+function ContadorApu()
+{
+    $fechainicial = date('Y-m-01');
+    $fechafinal = date('Y-m-t');
+
+    $con = new LocalConector();
+    $conex = $con->conectar();
+
+    $datos = mysqli_query($conex, "SELECT `NombreCurso` FROM `Cursos` WHERE `Fecha` BETWEEN '$fechainicial' and '$fechafinal' GROUP by `NombreCurso`");
+    echo"SELECT `NombreCurso` FROM `Cursos` WHERE `Fecha` BETWEEN '$fechainicial' and '$fechafinal' GROUP by `NombreCurso`";
+    $resultado = mysqli_fetch_all($datos, MYSQLI_ASSOC);
+    echo json_encode(array("data" => $resultado));
+}
+
+?>
