da189fb89410edee47668c81b99adb5c6c2499db
hadbet <hadbet.altamirano@grammer.com>
2024-01-23 17:33:29 -0600

    Carga inicial de plantilla

--- dao/daoCurso.php
+++ dao/daoCurso.php
@@ -6,10 +6,13 @@ ContadorApu();
 
 function ContadorApu()
 {
+    $fechainicial = date('Y-m-01');
+    $fechafinal = date('Y-m-t');
+
     $con = new LocalConector();
     $conex = $con->conectar();
 
-    $datos = mysqli_query($conex, "SELECT * FROM `UsuariosEntrevistas` WHERE `Puesto` like '%APU%';");
+    $datos = mysqli_query($conex, "SELECT `NombreCurso` FROM `Cursos` WHERE `Fecha` BETWEEN '$fechainicial' and '$fechafinal';");
 
     $resultado = mysqli_fetch_all($datos, MYSQLI_ASSOC);
     echo json_encode(array("data" => $resultado));
